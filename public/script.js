2ff9e718712f3521056dcdc7b5af9ce8a43682f1
igorracki <igor.racki@outlook.com>
2024-05-31 22:30:17 +0100

    added x and y axis

--- public/script.js
+++ public/script.js
@@ -4,8 +4,11 @@ document.getElementById('audiofile').addEventListener('change', function(event) 
     const fileReader = new FileReader()
     const fftSize = 2048
     const normalizationFactor = 128.0 // [0,255] == [-1,1]
-    const frequencyBarScaling = 3
-    const barHeightScale = 5
+    const frequencyBarScaling = 2
+    const barHeightScale = 4.5
+    const xLabelOffset = 30;
+    const yLabelOffset = 30;
+    const tickSize = 5;
 
     fileReader.onload = function() {
         audioContext.decodeAudioData(fileReader.result, function(buffer) {
@@ -25,6 +28,20 @@ document.getElementById('audiofile').addEventListener('change', function(event) 
             const waveformContext = waveformCanvas.getContext('2d');
             const fftCanvas = document.getElementById('fft');
             const fftContext = fftCanvas.getContext('2d');
+
+            const stopButton = document.getElementById('stopButton');
+            stopButton.disabled = false;
+            let isPlaying = true;
+
+            function stop() {
+                if (isPlaying) {
+                    source.stop();
+                    isPlaying = false;
+                    stopButton.disabled = true;
+                }
+            }
+
+            stopButton.addEventListener('click', stop);
 
             function drawWaveform() {
                 requestAnimationFrame(drawWaveform);
@@ -48,6 +65,7 @@ document.getElementById('audiofile').addEventListener('change', function(event) 
                 }
                 waveformContext.lineTo(waveformCanvas.width, waveformCanvas.height / 2);
                 waveformContext.stroke();
+                drawWaveformAxis();
             }
 
             function drawFFT() {
@@ -68,16 +86,107 @@ document.getElementById('audiofile').addEventListener('change', function(event) 
                     fftContext.fillRect(x, fftCanvas.height - barHeight / 2, barWidth, barHeight / 2);
                     x += barWidth + 1;
                 }
+                drawFFTAxis();
+            }
+
+
+            function drawWaveformAxis() {
+                waveformCanvas.strokeStyle = "white";
+                waveformContext.lineWidth = 1;
+
+                waveformContext.beginPath();
+                waveformContext.moveTo(0, waveformCanvas.height - 1);
+                waveformContext.lineTo(waveformCanvas.width, waveformCanvas.height - 1);
+                waveformContext.stroke();
+
+                waveformContext.beginPath();
+                waveformContext.moveTo(0, 0);
+                waveformContext.lineTo(0, waveformCanvas.height);
+                waveformContext.stroke();
+
+                waveformContext.fillStyle = "white";
+                waveformContext.font = '12px Consolas';
+                waveformContext.fillText('Time', waveformCanvas.width / 2, waveformCanvas.height - xLabelOffset);
+                waveformContext.save();
+                waveformContext.translate(10, waveformCanvas.height / 2);
+                waveformContext.rotate(-Math.PI / 2);
+                waveformContext.fillText('Amplitude', 0, 0);
+                waveformContext.restore();
+
+                const xTick = waveformCanvas.width / 10;
+                for (let i = 1; i <= 10; i++) {
+                    const x = i * xTick;
+                    waveformContext.beginPath();
+                    waveformContext.moveTo(x, waveformCanvas.height - tickSize);
+                    waveformContext.lineTo(x, waveformCanvas.height + tickSize);
+                    waveformContext.stroke();
+                    waveformContext.fillText((i * 0.1).toFixed(1), x - tickSize, waveformCanvas.height - tickSize);
+                }
+
+                const yTick = waveformCanvas.height / 4;
+                for (let i = 1; i <= 4; i++) {
+                    const y = i * yTick;
+                    waveformContext.beginPath();
+                    waveformContext.moveTo(-tickSize, y);
+                    waveformContext.lineTo(tickSize, y);
+                    waveformContext.stroke();
+                    waveformContext.fillText((1 - (i * 0.5)).toFixed(1), yLabelOffset, y - tickSize * 2);
+                }
+            }
+
+            function drawFFTAxis() {
+                fftContext.strokeStyle = 'white';
+                fftContext.lineWidth = 1;
+
+                fftContext.beginPath();
+                fftContext.moveTo(0, fftCanvas.height - 1);
+                fftContext.lineTo(fftCanvas.width, fftCanvas.height - 1);
+                fftContext.stroke();
+
+                fftContext.beginPath();
+                fftContext.moveTo(0, 0);
+                fftContext.lineTo(0, fftCanvas.height);
+                fftContext.stroke();
+
+                fftContext.fillStyle = 'white';
+                fftContext.font = '12px Consolas';
+                fftContext.fillText('Frequency', fftCanvas.width / 2, fftCanvas.height - xLabelOffset);
+                fftContext.save();
+                fftContext.translate(10, fftCanvas.height / 2);
+                fftContext.rotate(-Math.PI / 2);
+                fftContext.fillText('Magnitude', 0, 0);
+                fftContext.restore();
+
+                const xTick = fftCanvas.width / 10;
+                for (let i = 1; i <= 10; i++) {
+                    const x = i * xTick;
+                    fftContext.beginPath();
+                    fftContext.moveTo(x, fftCanvas.height - tickSize);
+                    fftContext.lineTo(x, fftCanvas.height + tickSize);
+                    fftContext.stroke();
+                    fftContext.fillText((i * (analyser.context.sampleRate / 2 / 10)).toFixed(0), x - tickSize, fftCanvas.height - tickSize);
+                }
+
+                const yTick = fftCanvas.height / 4;
+                for (let i = 1; i <= 4; i++) {
+                    const y = i * yTick;
+                    fftContext.beginPath();
+                    fftContext.moveTo(-tickSize, y);
+                    fftContext.lineTo(tickSize, y);
+                    fftContext.stroke();
+                    fftContext.fillText((1 - (i * 0.25)).toFixed(2), yLabelOffset, y - tickSize * 2);
+                }
             }
 
             function resizeCanvases() {
                 const width = window.innerWidth;
                 const height = window.innerHeight;
-                const padding = 50
-                waveformCanvas.width = width - padding;
-                waveformCanvas.height = (height / 2) - padding;
-                fftCanvas.width = width - padding;
-                fftCanvas.height = (height / 2) - padding;
+                const paddingTB = 60
+                const paddingLR = 20
+                waveformCanvas.width = width - paddingLR;
+                waveformCanvas.height = (height / 2) - paddingTB;
+                fftCanvas.width = width - paddingLR;
+                fftCanvas.height = (height / 2) - paddingTB;
             }
 
             window.addEventListener('resize', resizeCanvases);
